d9ffd461a8f70be8eca0d90ac9c4efdf7569c14c
Jason Hazel <jhazel@matrixmediaservices.com>
2022-05-29 15:48:52 -0400

    comments

--- keymaps/keymaps.h
+++ keymaps/keymaps.h
@@ -13,8 +13,7 @@ enum layers {
  };
 
 
-// this approach kind of breaks down on keyboards that support multiple layouts (ilke the corne)
-// need a way to force a layout from the keyboard keymap
+// not as 'magical' as I was hoping for, but the path of least resistence to handling boards with multiple supported layouts
 #ifdef KEYMAP_split_3x5_3
     #include "layouts/split_3x5_3.h"
     #define LAYOUT_BASE LAYOUT_split_3x5_3

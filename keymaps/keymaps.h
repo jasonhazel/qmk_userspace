10f0413fc955c5b948193253742982ef14617adc
Jason Hazel <jhazel@matrixmediaservices.com>
2022-05-29 15:24:00 -0400

    adding 3x6_3 layout

--- keymaps/keymaps.h
+++ keymaps/keymaps.h
@@ -12,16 +12,27 @@ enum layers {
     LAYER_LENGTH
  };
 
-#ifdef LAYOUT_split_3x5_3
+
+// this approach kind of breaks down on keyboards that support multiple layouts (ilke the corne)
+// need a way to force a layout from the keyboard keymap
+#ifdef KEYMAP_split_3x5_3
     #include "layouts/split_3x5_3.h"
     #define LAYOUT_BASE LAYOUT_split_3x5_3
 #endif
 
-#ifdef LAYOUT_split_3x5_2
+#ifdef KEYMAP_split_3x5_2
     #include "layouts/split_3x5_2.h"
     #define LAYOUT_BASE LAYOUT_split_3x5_2
 #endif
 
-#ifndef LAYOUT_BASE
-    #define LAYOUT_WRAPPER(...) LAYOUT_BASE(__VA_ARGS__)
+#ifdef KEYMAP_split_3x6_3
+    #include "layouts/split_3x6_3.h"
+    #define LAYOUT_BASE LAYOUT_split_3x6_3
 #endif
+
+
+#ifndef LAYOUT_BASE
+    #error USERSPACE LAYOUT MISSING
+#endif
+
+#define LAYOUT_WRAPPER(...) LAYOUT_BASE(__VA_ARGS__)

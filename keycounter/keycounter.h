1252f76f07d42b57db389e25b90a45d1c7964aaa
Jason Hazel <jhazel@matrixmediaservices.com>
2022-05-29 19:02:06 -0400

    fix keycode for reset

--- keycounter/keycounter.h
+++ keycounter/keycounter.h
@@ -3,9 +3,11 @@
 
 #ifdef KEYCOUNTER_ENABLE
     enum keycounter_keycodes {
-        KEY_COUNTER_TOGGLE = SAFE_RANGE, 
-        KEY_COUNTER_RESET
+        KEYCOUNTER_TOGGLE = SAFE_RANGE, 
+        KEYCOUNTER_RESET
     };
+
+    
 
     bool process_record_user_keycounter(uint16_t keycode, keyrecord_t *record);
     #ifdef OLED_ENABLE
